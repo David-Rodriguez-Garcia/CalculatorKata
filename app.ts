149b7d4953682ff23341a9a5b469d5143ccf9419
David Rodriguez Garcia <david@MacBook-Pro-de-infotec.local>
2022-04-20 20:57:30 +0200

    With errors, no correct output

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
-class StringCalculator {
+
+
+/*TODO - The input seems to be too large for us to handle it. We must check the values we are passing or the conditionals */
+export class StringCalculator {
     Add(nums: string): number{
         let arr = nums.split(',');
         //check that we got two numbers, and that both of those numbers are smaller than 15 characters long (number is max 16 digits)
         if (arr.length > 1 && arr.length <= 80){
             //check their length && that they are numbers && a security for max 80 elements of lenght 14-> Why? -> avoid bugs. Max number 16 digits
-            if (arr.every((elem) => elem[0].length < 14 && !isNaN(parseFloat(elem[0])))){
+            if (arr.every((elem) => elem.length < 14 && !isNaN(parseFloat(elem)))){
                 
-                let numAr: number[] = arr.map(elem => numAr.push(parseFloat(elem[0])));
-    
-                //check that they are both numbers
-                if (!isNaN(numAr[0]) && !isNaN(numAr[1])){
-                    //should throw errors -> return (numAr[0] + numAr[1] + 1234567890123456789012345678901234567890);
-                    return (numAr[0] + numAr[1]);
-                }
+                let numAr: number[] = arr.map(elem => parseFloat(elem[0]));
+                let result = numAr.reduce((acumulator, elem) => acumulator + elem, 0);
+                console.log('CLASS OUTPUT: ', result, ' - Class Input - ', numAr, ' ---- ', arr)
+                return (result);
             }
         }
         return 0;
--- tests.ts
+++ tests.ts
@@ -1,23 +1,23 @@
+import {StringCalculator} from "./app.js"
 //TEST CHECKER
-type AddCheckerFunc = (input: string, expectedOutput: number, index: number) => boolean;
+type AddCheckerFunc = (input: string, expectedOutput: number, index: number) => [string, boolean];
 
 const sc: StringCalculator = new StringCalculator()
 
-let trace: string = "ADD CHECKER TRACE\n\n";
 //returns true if no errors, false if errors
 const addChecker: AddCheckerFunc = (input, expectedOutput, index) => {
-    let error: boolean = false;
-    let realOutput = sc.Add(input);
     let txt: string;
+    let success = true;
+    let realOutput = sc.Add(input);
     if (realOutput === expectedOutput){
         txt = '\nTest n-' + index + ' CORRECT';
     } else {
         txt = '\nTest n-' + index + ' ERROR';
-        error = true;
+        numErrors++;
+        success = false;
     }
     txt += 'For input: ' + input + '\nExpected output: ' + expectedOutput + '\nCurrent output: ' + realOutput + '\n';
-    trace.concat('\n', txt)
-    return !error;
+    return [txt, success];
 }
 
 
@@ -30,20 +30,27 @@ function getRandomNumber(min: number, max: number): number {
 let myArr: [string, number][] = [['123,  351', 474], ['123,  ', 474], ['123,  &', 0]];
 const generateInput = () => {
     for (let i: number = 0; i < 100; i++) {
-        let fnum = getRandomNumber(-9000000000000000, 9000000000000000);
-        let snum = getRandomNumber(-9000000000000000, 9000000000000000);
+        let fnum = getRandomNumber(-90000000000, 90000000000);
+        let snum = getRandomNumber(-90000000000, 90000000000);
 
         myArr.push([fnum + ',' + snum, fnum + snum])
     }
 }
 
+generateInput();
+
 //const myArr: [string, number][] = [['', 0], ['1,2', 3], ['13475,0', 13475], ['0,13475', 13475], ['-13475, 3', -13472], ['9007199254740992,0', 0], ['4, 9007199254740990', 0], ['-9007199254740991, - 4', 0], ['-3, -5', -8], ['true', 0]]
 let numErrors: number = 0;
 let counter: number = 0;
+let trace: string = "ADD CHECKER TRACE\n\n";
+let txtError: string = "ADD CHECKER TRACE\n\n";
 for (let [input, output] of myArr){
-    if (!addChecker(input, output, counter)){
+    let [txt, success] = addChecker(input, output, counter)
+    if (!success){
         numErrors++;
+        txtError.concat('\n', txt);
     }
+    trace = trace.concat('\n', txt)
     counter++;
 }
 
